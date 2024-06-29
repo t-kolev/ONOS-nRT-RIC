52f0423f078baf192b27737dc52eb2ab3976d03d
t-kolev <cecok01@gmail.com>
2024-06-29 14:53:13 +0200

    changed push placement

--- update_repo.sh
+++ update_repo.sh
@@ -57,6 +57,5 @@ while IFS= read -r url; do
             echo "Subtree added successfully for URL: $url"
         fi
     fi
-    echo "Pushing subtree changes for URL: $url"
-    git subtree push --prefix "$local_dir" "$url" main
 done < "$URLS_FILE"
+git push 
